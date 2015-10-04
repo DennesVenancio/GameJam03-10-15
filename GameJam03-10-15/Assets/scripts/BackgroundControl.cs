a9ed932f8261ec4c6029b7a0978d103d4497c4de
thiagocgp <thiagocgp@gmail.com>
2015-10-04 02:07:08 -0300

    oi

---
+++ GameJam03-10-15/Assets/scripts/BackgroundControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundControl : MonoBehaviour {
+
+	public Material nuvemMaterial;
+	public Material nevoaMaterial;
+	float NuvemOffset;
+	float NevoaOffset;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		NuvemOffset += 0.0001f;
+		nuvemMaterial.SetTextureOffset ("_MainTex", new Vector2 (-1.0f * NuvemOffset, 0));
+
+		NevoaOffset += 0.0005f;
+		nevoaMaterial.SetTextureOffset ("_MainTex", new Vector2 (1.0f * NevoaOffset, 0));
+	}
+	
+}
---
+++ GameJam03-10-15/Assets/scripts/WaterController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterController : MonoBehaviour {
+
+	public Material agua1Material;
+	public Material agua2Material;
+	float agua1Offset;
+	float agua2Offset;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		agua1Offset += 0.005f;
+		agua1Material.SetTextureOffset ("_MainTex", new Vector2 (-1.0f * agua1Offset, 0));
+		
+		agua2Offset += 0.01f;
+		agua2Material.SetTextureOffset ("_MainTex", new Vector2 (1.0f * agua2Offset, 0));
+	}
+}
