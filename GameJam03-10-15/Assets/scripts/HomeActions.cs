f036e6aa8bcc57b8524ff33f6842bdf7dbe18a79
dennes.venancio <dennes.venancio@gmail.com>
2015-10-04 15:29:11 -0300

    Alterando algumas coisas

---
+++ GameJam03-10-15/Assets/scripts/HomeActions.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HomeActions : MonoBehaviour {
+
+	public Button play;
+	public Button sound;
+	public Button info;
+	public Button exit;
+
+
+	// Use this for initialization
+	void Start () {
+		play.onClick.AddListener(() => { playAction();});
+		sound.onClick.AddListener(() => { soundAction();});
+		info.onClick.AddListener(() => { infoAction();});
+		exit.onClick.AddListener(() => { exitAction();});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void playAction(){
+		Application.LoadLevel ("Level1");
+	}
+
+	private void soundAction(){
+
+	}
+
+	private void infoAction(){
+
+	}
+
+	private void exitAction(){
+
+	}
+}
